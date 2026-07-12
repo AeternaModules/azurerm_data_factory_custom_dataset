fa50815a6453b7965e3ab5b838cd5010a8411b89
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_custom_datasets_id" {
+  description = "Map of id values across all data_factory_custom_datasets, keyed the same as var.data_factory_custom_datasets"
+  value       = { for k, v in azurerm_data_factory_custom_dataset.data_factory_custom_datasets : k => v.id }
+}
 output "data_factory_custom_datasets_additional_properties" {
   description = "Map of additional_properties values across all data_factory_custom_datasets, keyed the same as var.data_factory_custom_datasets"
   value       = { for k, v in azurerm_data_factory_custom_dataset.data_factory_custom_datasets : k => v.additional_properties }
